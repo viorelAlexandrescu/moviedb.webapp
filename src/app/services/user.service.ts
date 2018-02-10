af5d29d295c8a1ab02e2434cf95fb53966db02b6
Stefan Anghele <stefananghelei@outlook.com>
2018-02-10 10:51:37 +0200

    should show none or username for username element

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -26,8 +26,7 @@ private url = '';
 
   getUsername() {
     const user = JSON.parse(localStorage.getItem('user'));
-    console.log('user is ', user);
-    return user === {} ? 'None' : user.username;
+    return user === null ? 'None' : user.username;
   }
 
   getDiscussions() {
